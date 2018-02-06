dde5d44a0b03d00809bb78651daef87f69f92a22
aedans <aedan.smith@outlook.com>
2018-02-06 15:34:05 -0800

    Add free and cofree conversions

--- src/main/kotlin/io/github/aedans/katalyst/fixedpoint/Cofree.kt
+++ src/main/kotlin/io/github/aedans/katalyst/fixedpoint/Cofree.kt
@@ -1,15 +1,31 @@
 package io.github.aedans.katalyst.fixedpoint
 
 import arrow.HK
+import arrow.core.*
+import arrow.free.Cofree
+import arrow.typeclasses.*
 import io.github.aedans.katalyst.data.*
+import io.github.aedans.katalyst.syntax.*
 
 typealias CofreePattern<S, A> = EnvTKindPartial<A, S>
 
 /**
  * Cofree comonad parameterized by a recursive type combinator.
  */
-typealias GCofree<T, S, A> = HK<T, CofreePattern<A, S>>
+typealias GCofree<T, S, A> = HK<T, CofreePattern<S, A>>
 
 typealias FixCofree<S, A> = GCofree<FixHK, S, A>
 typealias MuCofree<S, A> = GCofree<MuHK, S, A>
 typealias NuCofree<S, A> = GCofree<NuHK, S, A>
+
+fun <S, A> toGCofreeCoalgebra() = Coalgebra<CofreePattern<S, A>, Cofree<S, A>> {
+    EnvT(it.head toT it.tailForced())
+}
+
+fun <S, A> fromGCofreeAlgebra(FS: Functor<S>) = Algebra<CofreePattern<S, A>, Cofree<S, A>> {
+    val ev = it.ev()
+    Cofree(FS, ev.ask, Eval.later { ev.lower })
+}
+
+inline fun <reified T, S, A> Cofree<S, A>.toGCofree(): GCofree<T, S, A> = ana(coalg = toGCofreeCoalgebra())
+inline fun <reified T, reified S, A> GCofree<T, S, A>.toCofree(FS: Functor<S> = functor()): Cofree<S, A> = cata(alg = fromGCofreeAlgebra(FS))
--- src/main/kotlin/io/github/aedans/katalyst/fixedpoint/Free.kt
+++ src/main/kotlin/io/github/aedans/katalyst/fixedpoint/Free.kt
@@ -1,7 +1,12 @@
 package io.github.aedans.katalyst.fixedpoint
 
 import arrow.HK
+import arrow.core.*
+import arrow.free.*
+import arrow.free.Free.Pure
+import arrow.typeclasses.*
 import io.github.aedans.katalyst.data.*
+import io.github.aedans.katalyst.syntax.*
 
 typealias FreePattern<S, A> = CoEnvKindPartial<A, S>
 
@@ -13,3 +18,20 @@ typealias GFree<T, S, A> = HK<T, FreePattern<S, A>>
 typealias FixFree<S, A> = GFree<FixHK, S, A>
 typealias MuFree<S, A> = GFree<MuHK, S, A>
 typealias NuFree<S, A> = GFree<NuHK, S, A>
+
+fun <S, A> toGFreeCoalgebra(MS: Monad<S>) = Coalgebra<FreePattern<S, A>, Free<S, A>> { free ->
+    when (free) {
+        is Pure -> CoEnv(Left(free.a))
+        else -> CoEnv(Right(MS.map(free.run(MS)) { free.step() }))
+    }
+}
+
+fun <S, A> fromGFreeAlgebra() = Algebra<FreePattern<S, A>, Free<S, A>> {
+    it.ev().run.fold(
+            Free.Companion::pure,
+            { Free.monad<S>().flatten(Free.liftF(it)).ev() }
+    )
+}
+
+inline fun <reified T, reified S, A> Free<S, A>.toGFree(MS: Monad<S> = monad()): GFree<T, S, A> = ana(coalg = toGFreeCoalgebra(MS))
+inline fun <reified T, S, A> GFree<T, S, A>.toFree(): Free<S, A> = cata(alg = fromGFreeAlgebra())
