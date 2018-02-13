c4da5f975095b1267192477874db62499edebe08
aedans <aedan.smith@outlook.com>
2018-02-12 19:12:31 -0800

    Pull out advanced morphisms

--- src/main/kotlin/io/github/aedans/katalyst/typeclasses/Corecursive.kt
+++ src/main/kotlin/io/github/aedans/katalyst/typeclasses/Corecursive.kt
@@ -14,14 +14,14 @@ import io.github.aedans.katalyst.syntax.*
 @typeclass
 interface Corecursive<T> : TC {
     /**
-     * Creates a algebra given a functor.
-     */
-    fun <F> embed(FF: Functor<F>): Algebra<F, HK<T, F>> = { t: HK<F, HK<T, F>> -> embedT(t, FF) }
-
-    /**
      * Implementation for embed.
      */
     fun <F> embedT(t: HK<F, HK<T, F>>, FF: Functor<F>): HK<T, F>
+
+    /**
+     * Creates a algebra given a functor.
+     */
+    fun <F> embed(FF: Functor<F>): Algebra<F, HK<T, F>> = { t: HK<F, HK<T, F>> -> embedT(t, FF) }
 
     /**
      * Unfold into any recursive type.
@@ -30,60 +30,60 @@ interface Corecursive<T> : TC {
                    FF: Functor<F>): HK<T, F> =
             hylo(a, embed(FF), coalg,
                     FF)
-
-    /**
-     * Ana generalized over a monad.
-     */
-    fun <F, M, A> anaM(a: A, coalg: CoalgebraM<M, F, A>,
-                       TF: Traverse<F>, MM: Monad<M>): HK<M, HK<T, F>> =
-            hyloM(a, { MM.pure(embedT(it, TF)) }, coalg,
-                    TF, MM)
-
-    /**
-     * Ana generalized over a comonad.
-     */
-    fun <F, N, A> gana(a: A, dNF: DistributiveLaw<N, F>, coalg: GCoalgebra<N, F, A>,
-                       FF: Functor<F>, MN: Monad<N>): HK<T, F> =
-            ana(MN.pure(a), { FF.map(dNF.invoke(MN.map(it, coalg)), MN::flatten) },
-                    FF)
-
-    /**
-     * Ana generalized over a monad and a comonad.
-     */
-    fun <F, N, M, A> ganaM(a: A, dNF: DistributiveLaw<N, F>, coalg: GCoalgebraM<N, M, F, A>,
-                           TF: Traverse<F>, MN: Monad<N>, TN: Traverse<N>, MM: Monad<M>): HK<M, HK<T, F>> =
-            ghyloM(a, distCata(TF), dNF, { MM.pure(embedT(TF.map(it) { it.ev().value }, TF)) }, coalg,
-                    Id.comonad(), Id.traverse(), MN, TN, MM, TF)
-
-    /**
-     * Ana that stops on failure.
-     */
-    fun <F, A> apo(a: A, coalg: GCoalgebra<EitherKindPartial<HK<T, F>>, F, A>,
-                   FF: Functor<F>): HK<T, F> =
-            hylo(a, { embedT(FF.map(it.unnest()) { it.ev().merge() }, FF) }, { coalg(it).nest() },
-                    ComposedFunctor(FF, Either.functor<HK<T, F>>()))
-
-    /**
-     * Apo generalized over a monad.
-     */
-    fun <F, M, A> apoM(a: A, coalg: GCoalgebraM<EitherKindPartial<HK<T, F>>, M, F, A>,
-                       TF: Traverse<F>, MM: Monad<M>): HK<M, HK<T, F>> =
-            hyloM(a, { MM.pure(embedT(TF.map(TF.map(it.unnest()) { it.ev() }) { it.merge() }, TF)) }, { MM.map(coalg(it)) { it.nest() } },
-                    ComposedTraverse(TF, Either.traverse<HK<T, F>>(), Either.applicative()), MM)
-
-    /**
-     * Ana that also computes future elements.
-     */
-    fun <F, A> futu(a: A, coalg: GCoalgebra<FreeKindPartial<F>, F, A>,
-                    FF: Functor<F>): HK<T, F> =
-            gana(a, distFutu(FF), coalg,
-                    FF, Free.monad())
-
-    /**
-     * Futu generalized over a monad.
-     */
-    fun <F, M, A> futuM(a: A, coalg: GCoalgebraM<FreeKindPartial<F>, M, F, A>,
-                        MF: Monad<F>, TF: Traverse<F>, MM: Monad<M>): HK<M, HK<T, F>> =
-            ganaM(a, distFutu(MF), coalg,
-                    TF, Free.monad(), /* Free.traverse() */ TODO("Free.traverse()"), MM)
 }
+
+/**
+ * Ana generalized over a monad.
+ */
+fun <T, F, M, A> Corecursive<T>.anaM(a: A, coalg: CoalgebraM<M, F, A>,
+                   TF: Traverse<F>, MM: Monad<M>): HK<M, HK<T, F>> =
+        hyloM(a, { MM.pure(embedT(it, TF)) }, coalg,
+                TF, MM)
+
+/**
+ * Ana generalized over a comonad.
+ */
+fun <T, F, N, A> Corecursive<T>.gana(a: A, dNF: DistributiveLaw<N, F>, coalg: GCoalgebra<N, F, A>,
+                   FF: Functor<F>, MN: Monad<N>): HK<T, F> =
+        ana(MN.pure(a), { FF.map(dNF.invoke(MN.map(it, coalg)), MN::flatten) },
+                FF)
+
+/**
+ * Ana generalized over a monad and a comonad.
+ */
+fun <T, F, N, M, A> Corecursive<T>.ganaM(a: A, dNF: DistributiveLaw<N, F>, coalg: GCoalgebraM<N, M, F, A>,
+                       TF: Traverse<F>, MN: Monad<N>, TN: Traverse<N>, MM: Monad<M>): HK<M, HK<T, F>> =
+        ghyloM(a, distCata(TF), dNF, { MM.pure(embedT(TF.map(it) { it.ev().value }, TF)) }, coalg,
+                Id.comonad(), Id.traverse(), MN, TN, MM, TF)
+
+/**
+ * Ana that stops on failure.
+ */
+fun <T, F, A> Corecursive<T>.apo(a: A, coalg: GCoalgebra<EitherKindPartial<HK<T, F>>, F, A>,
+               FF: Functor<F>): HK<T, F> =
+        hylo(a, { embedT(FF.map(it.unnest()) { it.ev().merge() }, FF) }, { coalg(it).nest() },
+                ComposedFunctor(FF, Either.functor<HK<T, F>>()))
+
+/**
+ * Apo generalized over a monad.
+ */
+fun <T, F, M, A> Corecursive<T>.apoM(a: A, coalg: GCoalgebraM<EitherKindPartial<HK<T, F>>, M, F, A>,
+                   TF: Traverse<F>, MM: Monad<M>): HK<M, HK<T, F>> =
+        hyloM(a, { MM.pure(embedT(TF.map(TF.map(it.unnest()) { it.ev() }) { it.merge() }, TF)) }, { MM.map(coalg(it)) { it.nest() } },
+                ComposedTraverse(TF, Either.traverse<HK<T, F>>(), Either.applicative()), MM)
+
+/**
+ * Ana that also computes future elements.
+ */
+fun <T, F, A> Corecursive<T>.futu(a: A, coalg: GCoalgebra<FreeKindPartial<F>, F, A>,
+                FF: Functor<F>): HK<T, F> =
+        gana(a, distFutu(FF), coalg,
+                FF, Free.monad())
+
+/**
+ * Futu generalized over a monad.
+ */
+fun <T, F, M, A> Corecursive<T>.futuM(a: A, coalg: GCoalgebraM<FreeKindPartial<F>, M, F, A>,
+                    MF: Monad<F>, TF: Traverse<F>, MM: Monad<M>): HK<M, HK<T, F>> =
+        ganaM(a, distFutu(MF), coalg,
+                TF, Free.monad(), /* Free.traverse() */ TODO("Free.traverse()"), MM)
--- src/main/kotlin/io/github/aedans/katalyst/typeclasses/Recursive.kt
+++ src/main/kotlin/io/github/aedans/katalyst/typeclasses/Recursive.kt
@@ -13,14 +13,14 @@ import io.github.aedans.katalyst.syntax.*
 @typeclass
 interface Recursive<T> : TC {
     /**
-     * Creates a coalgebra given a functor.
-     */
-    fun <F> project(FF: Functor<F>): Coalgebra<F, HK<T, F>> = { t: HK<T, F> -> projectT(t, FF) }
-
-    /**
      * Implementation for project.
      */
     fun <F> projectT(t: HK<T, F>, FF: Functor<F>): HK<F, HK<T, F>>
+
+    /**
+     * Creates a coalgebra given a functor.
+     */
+    fun <F> project(FF: Functor<F>): Coalgebra<F, HK<T, F>> = { projectT(it, FF) }
 
     /**
      * Fold generalized over any recursive type.
@@ -29,60 +29,60 @@ interface Recursive<T> : TC {
                     FF: Functor<F>): A =
             hylo(t, alg, project(FF),
                     FF)
-
-    /**
-     * Cata generalized over a monad.
-     */
-    fun <F, M, A> cataM(t: HK<T, F>, alg: AlgebraM<M, F, A>,
-                        TF: Traverse<F>, MM: Monad<M>): HK<M, A> =
-            cata(t, { MM.flatMap(TF.sequence(MM, it), alg) },
-                    TF)
-
-    /**
-     * Cata generalized over a comonad.
-     */
-    fun <F, W, A> gcata(t: HK<T, F>, dFW: DistributiveLaw<F, W>, alg: GAlgebra<W, F, A>,
-                        FF: Functor<F>, CW: Comonad<W>): A =
-            CW.extract(cata(t, { CW.map(dFW.invoke(FF.map(it, CW::duplicate)), alg) },
-                    FF))
-
-    /**
-     * Cata generalized over a monad and a comonad.
-     */
-    fun <F, W, M, A> gcataM(t: HK<T, F>, dFW: DistributiveLaw<F, W>, alg: GAlgebraM<W, M, F, A>,
-                            TF: Traverse<F>, TW: Traverse<W>, MM: Monad<M>, CW: Comonad<W>): HK<M, A> =
-            MM.map<HK<W, A>, A>(cataM(t, { TW.traverse(dFW.invoke(TF.map(it, CW::duplicate)), alg, MM) },
-                    TF, MM), CW::extract)
-
-    /**
-     * Cata that also provides the previous element.
-     */
-    fun <F, A> para(t: HK<T, F>, alg: GAlgebra<PairKWKindPartial<HK<T, F>>, F, A>,
-                    FF: Functor<F>): A =
-            hylo(t, { alg(it.unnest()) }, { FF.map(projectT(it, FF), ::square).nest() },
-                    ComposedFunctor<F, PairKWKindPartial<HK<T, F>>>(FF, functor()))
-
-    /**
-     * Para generalized over a monad.
-     */
-    fun <F, M, A> paraM(t: HK<T, F>, alg: GAlgebraM<PairKWKindPartial<HK<T, F>>, M, F, A>,
-                        TF: Traverse<F>, MM: Monad<M>): HK<M, A> =
-            para(t, { MM.flatMap(TF.sequence(MM, TF.map(it) { traverse<PairKWKindPartial<HK<T, F>>>().sequence(MM, it) }), alg) },
-                    TF)
-
-    /**
-     * Cata that also provides all the previous elements.
-     */
-    fun <F, A> histo(t: HK<T, F>, alg: GAlgebra<CofreeKindPartial<F>, F, A>,
-                     FF: Functor<F>): A =
-            gcata(t, distHisto(FF), alg,
-                    FF, Cofree.comonad())
-
-    /**
-     * Histo generalized over a comonad.
-     */
-    fun <F, W, A> ghisto(t: HK<T, F>, dFW: DistributiveLaw<F, W>, alg: GAlgebra<CofreeKindPartial<W>, F, A>,
-                         FF: Functor<F>, FW: Functor<W>): A =
-            gcata(t, distGHisto(dFW, FF, FW), alg,
-                    FF, Cofree.comonad())
 }
+
+/**
+ * Cata generalized over a monad.
+ */
+fun <T, F, M, A> Recursive<T>.cataM(t: HK<T, F>, alg: AlgebraM<M, F, A>,
+                    TF: Traverse<F>, MM: Monad<M>): HK<M, A> =
+        cata(t, { MM.flatMap(TF.sequence(MM, it), alg) },
+                TF)
+
+/**
+ * Cata generalized over a comonad.
+ */
+fun <T, F, W, A> Recursive<T>.gcata(t: HK<T, F>, dFW: DistributiveLaw<F, W>, alg: GAlgebra<W, F, A>,
+                    FF: Functor<F>, CW: Comonad<W>): A =
+        CW.extract(cata(t, { CW.map(dFW.invoke(FF.map(it, CW::duplicate)), alg) },
+                FF))
+
+/**
+ * Cata generalized over a monad and a comonad.
+ */
+fun <T, F, W, M, A> Recursive<T>.gcataM(t: HK<T, F>, dFW: DistributiveLaw<F, W>, alg: GAlgebraM<W, M, F, A>,
+                        TF: Traverse<F>, TW: Traverse<W>, MM: Monad<M>, CW: Comonad<W>): HK<M, A> =
+        MM.map<HK<W, A>, A>(cataM(t, { TW.traverse(dFW.invoke(TF.map(it, CW::duplicate)), alg, MM) },
+                TF, MM), CW::extract)
+
+/**
+ * Cata that also provides the previous element.
+ */
+fun <T, F, A> Recursive<T>.para(t: HK<T, F>, alg: GAlgebra<PairKWKindPartial<HK<T, F>>, F, A>,
+                FF: Functor<F>): A =
+        hylo(t, { alg(it.unnest()) }, { FF.map(projectT(it, FF), ::square).nest() },
+                ComposedFunctor<F, PairKWKindPartial<HK<T, F>>>(FF, functor()))
+
+/**
+ * Para generalized over a monad.
+ */
+fun <T, F, M, A> Recursive<T>.paraM(t: HK<T, F>, alg: GAlgebraM<PairKWKindPartial<HK<T, F>>, M, F, A>,
+                    TF: Traverse<F>, MM: Monad<M>): HK<M, A> =
+        para(t, { MM.flatMap(TF.sequence(MM, TF.map(it) { traverse<PairKWKindPartial<HK<T, F>>>().sequence(MM, it) }), alg) },
+                TF)
+
+/**
+ * Cata that also provides all the previous elements.
+ */
+fun <T, F, A> Recursive<T>.histo(t: HK<T, F>, alg: GAlgebra<CofreeKindPartial<F>, F, A>,
+                 FF: Functor<F>): A =
+        gcata(t, distHisto(FF), alg,
+                FF, Cofree.comonad())
+
+/**
+ * Histo generalized over a comonad.
+ */
+fun <T, F, W, A> Recursive<T>.ghisto(t: HK<T, F>, dFW: DistributiveLaw<F, W>, alg: GAlgebra<CofreeKindPartial<W>, F, A>,
+                     FF: Functor<F>, FW: Functor<W>): A =
+        gcata(t, distGHisto(dFW, FF, FW), alg,
+                FF, Cofree.comonad())
